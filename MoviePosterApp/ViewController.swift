ec5c486e9de2b2638480a58f5b1e828640216ac9
Jason Mach <jasonmach@doans-macbook-air.local>
2019-08-06 22:19:58 +1000

    Initial Commit

---
+++ MoviePosterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePosterApp
+//
+//  Created by Jason Mach on 8/6/19.
+//  Copyright © 2019 Jason Mach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
